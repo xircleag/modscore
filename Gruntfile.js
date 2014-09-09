388e5f0de0b5662bb50e35d98aa7052641630d6b
Michael Kantor <michaelkantor@Michaels-MacBook-Pro.local>
2014-09-09 15:54:17 -0700

    Adds build capability  1. using uglify generates build/*.min.js  2. using concat generates build/*.js  3. using watch, now runs test runner on save  4. sets default grunt task to build and test

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,17 +1,52 @@
 module.exports = function(grunt) {
 
-    grunt.initConfig({
-	  jasmine: {
-	    overscore: {
-	      src: ["overscore.js", 'events.js', 'model.js'], // change this; browserify should generate this file on the test server. ?
-	      options: {
-	        specs: 'jasmine/spec/*Spec.js',
-	        summary: true
-	      }
-	    }
-	  }
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		jasmine: {
+			overscore: {
+				src: ["overscore.js", 'events.js', 'model.js'], // change this; browserify should generate this file on the test server. ?
+				options: {
+					specs: 'jasmine/spec/*Spec.js',
+					summary: true
+				}
+			},
+		},
+		uglify: {
+			options: {
+				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
+					'<%= grunt.template.today("yyyy-mm-dd") %> */',
+				mangle: {
+					except: ["m_"]
+				},
+				sourceMap: true
+			},
+			overscore: {
+				files: {
+					'build/<%= pkg.name %>.min.js': ['<%= jasmine.overscore.src %>']
+				}
+			}
+		},
+		concat: {
+			options: {
+				separator: ';'
+			},
+			dist: {
+				src: ['<%= jasmine.overscore.src %>'],
+				dest: 'build/<%= pkg.name %>.js'
+			}
+		},
+		watch: {
+		  	files: ['<%= jasmine.overscore.src %>', '<%= jasmine.overscore.options.specs %>'],
+		  	tasks: ['jasmine', 'uglify']
+		}
+
 	});
 
-	//https://github.com/gruntjs/grunt-contrib-jasmine
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-jasmine');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-concat');
+
+  	grunt.registerTask('default', ['uglify', 'concat', 'jasmine']);
+
 };
