fc693b4faaeca4f93d83033632bbe67d16eb194a
Michael Kantor <michael@layer.com>
2014-09-11 13:20:29 -0700

    Adds grunt task for building Readme from jsdoc header

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,7 +1,27 @@
 module.exports = function(grunt) {
 
+	grunt.registerMultiTask("buildGitReadme", "Replace specified file with matching text", function() {
+	    var paths = grunt.file.expand( this.data.paths ),
+	        out = this.data.output,
+	        contents = this.data.prepend;
+
+	        /* Don't expect multiple paths, but its boilerplate code I found which we can build upon */
+	    paths.forEach(function( path ) {
+	    	var tmpContents = grunt.file.read(path);
+	    	if (tmpContents.match(/START-GIT-README/)) {
+	    		tmpContents = tmpContents.replace(/^[\s\S]*?START-GIT-README([\s\S]*?)END-GIT-README[\s\S]*$/m, "$1");
+	    		tmpContents = tmpContents.replace(/^\s*\*/mg,"");
+	    		contents += tmpContents;
+	    	}
+	    });
+
+	    grunt.file.write( out, contents );
+	});
+
+
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
+		readme: String(grunt.file.read("model.js")).replace(/^[\s\S]*?START-GIT-README([\s\S]*?)END-GIT-README[\s\S]*$/m, "$1"),
 		jasmine: {
 			overscore: {
 				src: ["overscore.js", 'events.js', 'model.js'], // change this; browserify should generate this file on the test server. ?
@@ -14,7 +34,7 @@ module.exports = function(grunt) {
 		uglify: {
 			options: {
 				banner: '/*! <%= pkg.name %> - v<%= pkg.version %> - ' +
-					'<%= grunt.template.today("yyyy-mm-dd") %> */',
+					'<%= grunt.template.today("yyyy-mm-dd") %> */ ' ,
 				mangle: {
 					except: ["m_"]
 				},
@@ -51,9 +71,16 @@ module.exports = function(grunt) {
 		        }
 		    }
 		},
+		buildGitReadme: {
+			overscore: {
+				prepend: "<%= pkg.name %>\n========\n\n",
+				paths: ["model.js"],
+				output: "README.md"
+			}
+		},
 		watch: {
 		  	files: ['<%= jasmine.overscore.src %>', '<%= jasmine.overscore.options.specs %>', "Gruntfile.js"],
-		   	tasks: ['concat', 'jasmine', 'jsduck'] /* Concat lets us test in our local dev env and won't get done if we run/fail tests first. */
+		   	tasks: ['concat', 'jasmine', 'jsduck', 'buildGitReadme'] /* Concat lets us test in our local dev env and won't get done if we run/fail tests first. */
 		}
 	});
 
@@ -63,8 +90,8 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-concat');
 	grunt.loadNpmTasks('grunt-jsduck');
 
-  	grunt.registerTask('default', ['uglify', 'concat', 'jasmine', 'jsdoc']);
+  	grunt.registerTask('default', ['uglify', 'concat', 'jasmine', 'jsdoc', 'buildGitReadme']);
 
-  	grunt.registerTask('jenkins', ['uglify', 'concat', 'jasmine', 'jsdoc']);
+  	grunt.registerTask('jenkins', ['uglify', 'concat', 'jasmine', 'jsdoc', 'buildGitReadme']);
 
 };
