49447de9b5f1f7d27bcbd9eb37e68ab947f0584e
Michael Kantor <michael@layer.com>
2014-09-23 13:05:09 -0700

    Updates for saucelabs jasmine tests

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,4 @@
+/* Info on debugging grunt at https://gist.github.com/kagemusha/5866759 */
 module.exports = function(grunt) {
 
 	/* Experimental code for disabling audible alerts on error;
@@ -37,9 +38,16 @@ module.exports = function(grunt) {
 	    });
 	});
 
-
+	var credentials;
+	try {
+		credentials = grunt.file.readJSON('credentials.json');
+	} catch(e) {
+		console.warn("You do not have a credentials.json file; saucelabs builds will not be available");
+		credentials = {};
+	}
 
 	grunt.initConfig({
+		credentials: credentials,
 		pkg: grunt.file.readJSON('package.json'),
 		jasmine: {
 			modscore: {
@@ -131,14 +139,98 @@ module.exports = function(grunt) {
 			    	warning_level: "QUIET"
 			    }
 			},
-			layerjs: {
+			modscore: {
 				src: ['js/*.js'],
 				dest: "tmp/tmp.js"
 			}
 		},
+		connect: {
+			server: {
+		        options: {
+		            port: 9023
+		        }
+		    }
+	    },
+		'saucelabs-jasmine': { 
+			  modscore: {
+			    options: {
+			      username: credentials.saucelabs.user,
+			      key: credentials.saucelabs.pass,
+			      urls: ['http://127.0.0.1:9023/jasmine/SpecRunner.html'],
+			      //build: process.env.CI_BUILD_NUMBER,
+			      testname: 'Sauce Unit Test for modscore',
+			      browsers: [
+			      	{
+					    browserName: "internet explorer",
+					    platform: "WIN8",
+					    version: "10"
+					},
+					{
+				        browserName: 'firefox',
+				        version: '19',
+				        platform: 'XP'
+			        }/*,
+			        {
+			        	browserName: 'iphone',
+			        	version: '7.1',
+						platform: 'OS X 10.9'
+					},
+					{
+						browserName: 'android',
+			        	version: '4.4',
+						platform: 'Linux'
+					},
+					{
+						browserName: 'safari',
+			        	version: '7',
+						platform: 'OS X 10.9'
+
+					}*/
+			      ],
+
+					/* Result looks like:
+					{
+						id: "3f462dc7015342138bc02a76f1424691",
+						job_id: "e893247296014130838963db162a8dee",
+						passed: false,
+						platform: ["XP", "firefox","19"],
+						result: null,
+						testPageUrl: "http://127.0.0.1:9023/jasmine/SpecRunner.html",
+						url: "https://saucelabs.com/jobs/e893247296014130838963db162a8dee"
+					}
+				*/
+
+			      onTestComplete: function(result, callback) {
+			      	console.dir(result);
+			      	  var user = "mkantor_layer_com";
+        			  var pass = "7fe4f738-4780-41f4-b8e3-58a034eed2c5";
+			      	  require("request").put({
+				            url: ['https://saucelabs.com/rest/v1', user, 'jobs', result.job_id].join('/'),
+				            auth: { user: user, pass: pass },
+				            json: { passed: Boolean(result.passed) }
+				      }, function (error, response, body) {
+				      	if (response.statusCode != 200) {
+				      		console.error("Error updating sauce results: " + body.error);
+				      	}
+				      });
+
+			      	  console.log("Test Complete: " + Boolean(result.passed) + " for " + result.platform.join(" "));
+			      	  if (result.passed) {
+			      	  	console.log("Passed Tests: " + result.platform.join(" "));
+			      	  	callback();
+			      	  } else {
+			      	  	callback(new Error(result.platform.join(" ") + " failed tests; results at " + result.url));
+			      	  }
+
+			      }
+			    }
+			  }
+		},
 		watch: {
-		  	files: ['js/*.js', '<%= jasmine.modscore.options.specs %>', "Gruntfile.js"],
-		   	tasks: ['closureCompiler', 'browserify', 'jasmine', 'jsduck', 'buildGitReadme'] /* Concat lets us test in our local dev env and won't get done if we run/fail tests first. */
+			modscore: {
+			  	files: ['js/*.js', '<%= jasmine.modscore.options.specs %>', "Gruntfile.js"],
+			   	tasks: ['closureCompiler', 'browserify', 'jasmine', 'jsduck', 'buildGitReadme'] /* Concat lets us test in our local dev env and won't get done if we run/fail tests first. */
+			}
 		}
 	});
 
@@ -150,9 +242,15 @@ module.exports = function(grunt) {
  	grunt.loadNpmTasks('grunt-notify');
 	grunt.loadNpmTasks('grunt-bump');
 	grunt.loadNpmTasks('grunt-closure-tools');
+	grunt.loadNpmTasks('grunt-contrib-connect');
+	grunt.loadNpmTasks('grunt-saucelabs');
+
 
   	grunt.registerTask('default', ['closureCompiler', 'browserify', 'jasmine', 'jsduck', 'buildGitReadme', 'uglify', 'removeDebuggers']);
 
   	grunt.registerTask('jenkins', ['browserify', 'jasmine', 'jsduck', 'buildGitReadme', 'uglify', 'removeDebuggers']);
+	//grunt.registerTask("sauce", ["connect", "watch"]);
+
+	grunt.registerTask('sauce', ['connect', 'saucelabs-jasmine']);
 
 };
